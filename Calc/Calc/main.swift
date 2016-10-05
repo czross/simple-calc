0e3a55361d4aed2546ba1fe3553d62e05c0e0397
czross <czr@uw.edu>
2016-10-04 20:42:13 -0700

    most working but not %

--- Calc/Calc/main.swift
+++ Calc/Calc/main.swift
@@ -12,19 +12,36 @@ print("Enter all the numbers that you wish to operate on and then the operand at
 
 let response = readLine(strippingNewline: true)!
 
-if (response == nil) {
+if (response == nil || response.isEmpty) {
     print("please enter something")
 } else {
     var allArray = response.characters.split(whereSeparator: { $0 == " " })
         .map(String.init)
     var arrayLength = allArray.count
+    var answer: Double = 0
     if (allArray[arrayLength - 1] == "+" || allArray[arrayLength - 1] == "sum") {
-        var sum: Double = 0
         for i in 0...(arrayLength - 2) {
             var num = Double.init(allArray[i])
-            sum += num!
+            answer += num!
         }
-        print(sum)
+    } else if (allArray[arrayLength - 1] == "-" ){
+        for i in 0...(arrayLength - 2) {
+            var num = Double.init(allArray[i])
+            answer -= num!
+        }
+    } else if (allArray[arrayLength - 1] == "/" ) {
+        answer += (Double.init(allArray[0])! / Double.init(allArray[1])!)
+    } else if (allArray[arrayLength - 1] == "*" ) {
+        answer += (Double.init(allArray[0])! * Double.init(allArray[1])!)
+    } else if (allArray[arrayLength - 1] == "%" ) {
+        answer += (Double.init(allArray[0])! % Double.init(allArray[1])!)
+    } else if (allArray[arrayLength - 1] == "avg" ) {
+        for i in 0...(arrayLength - 2) {
+            var num = Double.init(allArray[i])
+            answer += num!
+            answer = answer / Double.init(arrayLength - 2)
+        }
     }
+    print(answer)
 }
 
